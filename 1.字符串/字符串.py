21fc126ece9637e0737b3eda9a8347f127d4e23a
AcherStyx <34408948+AcherStyx@users.noreply.github.com>
2018-12-25 16:01:09 +0800

    1.字符串
    简单的使用练习

---
+++ 1.字符串/字符串.py
@@ -0,0 +1,15 @@
+print("Hello World")
+print("Hello" " World") #字符串串联
+print("Hello"+" World")
+print("Hello"+" World"*2) #适合乘法运算
+print('Hello World') #单引号
+print(2*2)
+print(2.0*2)
+print(2*2.0) #和C中的类型转换不太一样
+x=2
+y=2.0
+print(int(x)*float(y))
+m=input("MUN1:")
+n=input("NUM2:")
+print((int(m)+int(n)))
+if m==n:print("相等")
---
+++ 2.简单运算/简单运算.py
@@ -0,0 +1,6 @@
+from math import *
+x=sqrt(2)
+print(float(x))
+print(int(x))
+print(int(-x))
+print(x)
