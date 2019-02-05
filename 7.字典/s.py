94c16708851df61785000285c104172c10f360ab
AcherStyx <34408948+AcherStyx@users.noreply.github.com>
2019-02-05 22:42:02 +0800

    7.字典

--- 7.字典/s.py
+++ 7.字典/s.py
@@ -26,6 +26,7 @@ data['more']['l2']=123
 print(data)
 
 #方法
+#copy
 diceg1=data.copy()
 diceg2=data
 print(diceg1)
@@ -34,4 +35,40 @@ diceg1['extra']=0
 print(data)
 diceg2['extra']=0
 print(data) #若使用直接复制，则替换时也会改变原件
-
+#clear
+diceg1.clear()
+print(diceg1)
+#fromkeys
+eg={}.fromkeys(['name','phone','adress'],'N/A')
+print(eg)
+eg={}
+eg.fromkeys(['name','phone','adress'],'N/A') #fromkeys不就地修改字典
+print(eg)
+#get
+eg={}.fromkeys(['name','phone','adress'],'N/A')
+print(eg.get('name'))
+print(eg.get('N/A'))
+#item
+print(eg.items()) #返回一个可迭代的字典视图
+eg['name']='Kaede'
+print(eg.items()) #会同步修改
+#key
+print(eg.keys())
+#pop
+eg.pop('adress')
+print(eg)
+#popitem
+eg.popitem() #随机弹出一个
+print(eg)
+#setdefault
+eg.setdefault('phone','N/A')
+print(eg)
+eg.setdefault('name','N/A') #键已经存在时，不会更改
+print(eg)
+#update
+change={'name':'qb','adress':'hok'}
+eg.update(change)
+print(eg)
+#value
+print(eg.values())
+print(list(eg.values()))
