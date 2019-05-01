1c606b82d26f6cd8084e326abcd3a591e3e95a59
AcherStyx <34408948+AcherStyx@users.noreply.github.com>
2019-05-01 10:41:01 +0800

    [ML][update] CIFAR-10 55% Acc

--- 机器学习/8.CIFAR-10 Large/NeuralNetwork.py
+++ 机器学习/8.CIFAR-10 Large/NeuralNetwork.py
@@ -1,14 +1,17 @@
 import tensorflow as tf
+import time
+import matplotlib.pyplot as plt
+import numpy as np
 
-def CNN_Interface(data,Layers,lineshape=True,bias=True,order=0,stddev=0.1,const_init=0.0):
+def CNN_Interface(data,Layers,lineshape=True,bias=True,order=0,stddev=0.1,const_init=0.0,active_function=tf.nn.tanh):
     '''
     传入初始的数据，以及按格式创建的各个层的参数，就可以完成卷积层和池化层的创建
     Layers示例:
     CNN_LAYERS=[
-        ["conv",[3,3,3,32],[1,1,1,1],True,"SAME"],
+        ["conv",[3,3,3,32],[1,1,1,1],0.01,"SAME",True],
         ["pool",[1,8,8,1],[1,8,8,1]],
         ]
-    卷积层：卷积层标识|过滤器大小|过滤器移动步长|是否使用偏置项|边界处理方式|
+    卷积层：卷积层标识|过滤器大小|过滤器移动步长|stddev|边界处理方式|是否使用偏置项|
     池化层：池化层标识|池化层大小|池化层移动步长|
     其他参数：
     order 当前是第几次条用本函数，多以调用需要更改order以防止变量名重复
@@ -20,20 +23,25 @@ def CNN_Interface(data,Layers,lineshape=True,bias=True,order=0,stddev=0.1,const_
         for layer_order,layer in enumerate(Layers):
             if layer[0]=="conv":
                 #过滤器
-                cnn_filter=tf.get_variable("conv{layer_order}_filter".format(layer_order=layer_order),shape=layer[1],initializer=tf.random_normal_initializer(stddev=stddev))
+                cnn_filter=tf.get_variable("conv{layer_order}_filter".format(layer_order=layer_order),shape=layer[1],initializer=tf.random_normal_initializer(stddev=layer[3]))
                 tf.summary.histogram("conv{layer_order}_filter".format(layer_order=layer_order), cnn_filter)
                 temp_layer=tf.nn.conv2d(hidden_layer,cnn_filter,layer[2],padding="SAME")
-                if bias==True:
+                if layer[5]==True:
                     #偏置项
                     cnn_bias=tf.get_variable("conv{layer_order}_biase".format(layer_order=layer_order),shape=[layer[1][3]],initializer=tf.constant_initializer(const_init))
                     tf.summary.histogram("conv{layer_order}_bias".format(layer_order=layer_order), cnn_bias)
                     temp_bias=tf.nn.bias_add(temp_layer,cnn_bias)
-                    hidden_layer=tf.nn.relu(temp_bias)
+                    hidden_layer=active_function(temp_bias)
                 else:
-                    hidden_layer=tf.nn.relu(temp_layer)
-            else:
+                    hidden_layer=active_function(temp_layer)
+            elif layer[0]=="maxpool":
                 temp_layer=tf.nn.max_pool(hidden_layer,ksize=layer[1],strides=layer[2],padding="SAME")
-                hidden_layer=tf.nn.relu(temp_layer)
+                hidden_layer=active_function(temp_layer)
+            elif layer[0]=="avgpool":
+                temp_layer=tf.nn.avg_pool(hidden_layer,ksize=layer[1],strides=layer[2],padding="SAME")
+                hidden_layer=active_function(temp_layer)
+            else:
+                raise(ValueError)
     if lineshape==True:
         pool_shape=hidden_layer.get_shape().as_list()
         nodes=pool_shape[1]*pool_shape[2]*pool_shape[3]
@@ -43,6 +51,14 @@ def CNN_Interface(data,Layers,lineshape=True,bias=True,order=0,stddev=0.1,const_
         return hidden_layer
 
 def FC_Interface(data,Layer,order=0,keep_prob_layer=1,keep_prob_image=1,stddev=0.1,const_init=0.0):
+    '''
+    自动创建全连接层
+    Layer示例：
+    [输入图片大小,隐藏层大小,...,输出层大小]
+    其他参数：
+    order：为防止重复预留的区分数字
+    keep_prob_layer,keep_prob_image：dropout操作时图像和隐藏层保留的比例，默认为1
+    '''
     with tf.variable_scope("Fully_Lincked_Networks_{order}".format(order=order)):
         #建立权重
         weight=[]
@@ -59,10 +75,10 @@ def FC_Interface(data,Layer,order=0,keep_prob_layer=1,keep_prob_image=1,stddev=0
         data=tf.nn.dropout(data,keep_prob=keep_prob_image)
         for i in range(len(Layer)-1):
             if i==0:
-                result=tf.nn.relu(tf.matmul(data,weight[i]))
+                result=tf.nn.tanh(tf.matmul(data,weight[i]))
             else:
                 result_droped=tf.nn.dropout(result,keep_prob=keep_prob_layer)
-                result=tf.nn.relu(tf.matmul(result_droped,weight[i]))
+                result=tf.nn.tanh(tf.matmul(result_droped,weight[i]))
     return result
 
 def Matedata_Writer(writer,feed_dict,train_op,step,checkfreq=1,sess="default",name="step{global_step}"):
@@ -72,42 +88,133 @@ def Matedata_Writer(writer,feed_dict,train_op,step,checkfreq=1,sess="default",na
     检查频率标志了多少次step时进行一次记录
     可利用name自定义当前次训练的命名
     '''
-    if step%checkfreq != 0:
+    with tf.variable_scope("Matedata_Writer"):
+        if step%checkfreq != 0:
+            return
+        if sess=="default":
+            sess=tf.get_default_session()
+        summary = tf.summary.merge_all()
+        runop=tf.RunOptions(trace_level=3)
+        runme=tf.RunMetadata()
+        summary_ , _ =sess.run([summary,train_op],feed_dict=feed_dict,run_metadata=runme,options=runop)
+        writer.add_run_metadata(runme,name.format(global_step=step))
+        writer.add_summary(summary_,step)
+
+def Softmax_Cross_Encropy_With_Regularization(label,logits,regularization_rate=0.0,single_index=False,collections=None,):
+    '''
+    接受label和logit计算交叉熵
+    在collections不为空的时候会同时计算正则化的损失，需要传入进行正则化计算的collenction
+    collections格式为：
+    [["collection_name_1"],
+    ["collection_name_1"],
+    ...]
+    '''
+    with tf.variable_scope("Loss"):
+        if single_index==True:
+            cross_entropy=tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(labels=label,logits=logits))
+        elif single_index==False:
+            cross_entropy=tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=label,logits=logits))
+        regu=tf.constant(0.0)
+        regularizer=tf.contrib.layers.l2_regularizer(regularization_rate)
+        if collections!=None:
+            for collection in collections:
+                for weight in tf.get_collection(collection):
+                    regu+=regularizer(weight)
+        return regu+cross_entropy,cross_entropy,regu
+
+def Info_Printer(learning_rate_base=None,learning_rate_decay_rate=None,learning_rate_decay_step=None,regularization_rate=None,keep_prob_layer=None,keep_prob_image=None):
+    if learning_rate_base!=None:
+        print("Learning rate base:{lrb}".format(lrb=learning_rate_base))
+    if learning_rate_decay_rate!=None:
+        print("Learning rate decay rate:{lrdr}".format(lrdr=learning_rate_decay_rate))
+    if learning_rate_decay_step!=None:
+        print("Learning rate decay step:{lrds}".format(lrds=learning_rate_decay_step))
+    if regularization_rate!=None:
+        print("Regularization rate:{regu}".format(regu=regularization_rate))
+    if keep_prob_layer!=None:
+        print("Keep prob layer:{drop}".format(drop=keep_prob_layer))
+    if keep_prob_image!=None:
+        print("Keep prob image:{drop}".format(drop=keep_prob_image))
+
+def Learning_Rate_Search(lr_tf_variable,train_op,accuracy_test_op,dataset,lr_base,lr_upper_bond=1,lr_raise_rate=1.5,train_step=5000,train_batch_size=1,test_batch_size=500,show_graphe=True,print_data=False,sess="default"):
+    if lr_base>lr_upper_bond:
         return
     if sess=="default":
         sess=tf.get_default_session()
-    summary = tf.summary.merge_all()
-    runop=tf.RunOptions(trace_level=3)
-    runme=tf.RunMetadata()
-    summary_ , _ =sess.run([summary,train_op],feed_dict=feed_dict,run_metadata=runme,options=runop)
-    writer.add_run_metadata(runme,name.format(global_step=step))
-    writer.add_summary(summary_,step)
+    lr_plotline=[]
+    lr_plotaxis=[]
+    feed_dict_test=dataset.nextbatch(test_batch_size,type="Test")
+    try:
+        while(1):
+            sess.run(tf.global_variables_initializer())
+            update_lr=tf.assign(lr_tf_variable,lr_base)
+            sess.run(update_lr)
+            for step in range(train_step):
+                feed_dict_train=dataset.nextbatch(train_batch_size)
+                sess.run(train_op,feed_dict=feed_dict_train)
+            lr_plotline.append(sess.run(accuracy_test_op,feed_dict=feed_dict_test))
+            print(lr_plotline[-1],sess.run(lr_tf_variable))
+            lr_plotaxis.append(lr_base)
+            if lr_base > lr_upper_bond:
+                break
+            lr_base=lr_base*lr_raise_rate
+    except KeyboardInterrupt:
+        pass
+
+    if show_graphe==True:
+        x=np.array(lr_plotaxis)
+        x=np.log10(x)
+        y=np.array(lr_plotline)
+        plt.plot(x,y,'r--',label='Learning rate')
+        plt.legend()
+        plt.show()
+    
+    return lr_plotaxis[np.argmax(lr_plotline)]
+
+
+
+class TrainTimer():
+    """
+    训练时计时用，setting设置时限
+    在到达时限时，返回1，否则0
+    若设置为-1.则无限制
+    """
+    limit=0
+    start_time=0
+    def __init__(self,timelimit=-1):
+        self.limit=timelimit
+    def setting(self):
+        self.start_time=time.time()
+    def check(self):
+        time_check=time.time()
+        if (time_check-self.start_time) > self.limit | self.limit!=-1:
+            return 1
+        else:
+            return 0
+    def getinput(self):
+        reply=input('Set time limit(s, unlimited: -1): ')
+        try:
+            self.limit=int(reply)
+        except ValueError:
+            self.limit=-1
 
 
 if __name__ == "__main__":
-    
     print("=========================")
     import random
     import numpy as np
     TEST_LAYERS=[
-        ["conv",[5,5,3,8],[1,1,1,1],True,"SAME"],
-        ["pool",[1,2,2,1],[1,2,2,1]],
-        ["conv",[5,5,8,32],[1,1,1,1],True,"SAME"],
-        ["conv",[5,5,32,32],[1,1,1,1],True,"SAME"],
-        ["conv",[5,5,32,32],[1,1,1,1],True,"SAME"],
-        ["conv",[5,5,32,32],[1,1,1,1],True,"SAME"],
-        ["conv",[5,5,32,32],[1,1,1,1],True,"SAME"],
-        ["conv",[5,5,32,32],[1,1,1,1],True,"SAME"],
-        ["conv",[5,5,32,32],[1,1,1,1],True,"SAME"],
-        ["conv",[5,5,32,32],[1,1,1,1],True,"SAME"],
-        ["conv",[5,5,32,32],[1,1,1,1],True,"SAME"],
-        ["conv",[5,5,32,32],[1,1,1,1],True,"SAME"],
-        ["conv",[5,5,32,32],[1,1,1,1],True,"SAME"],
-        ["pool",[1,2,2,1],[1,2,2,1]],
+        ["conv",[5,5,3,32],[1,1,1,1],0.0001,"SAME",True],
+        ["maxpool",[1,3,3,1],[1,2,2,1]],
+        ["conv",[5,5,32,32],[1,1,1,1],0.01,"SAME",True],
+        ["avgpool",[1,3,3,1],[1,2,2,1]],
+        ["conv",[5,5,32,64],[1,1,1,1],0.01,"SAME",True],
+        ["avgpool",[1,3,3,1],[1,2,2,1]],
     ]
     
     image=tf.Variable(tf.random_normal([1000,32,32,3],mean=1.0))
     print(image)
+    
     result,index=CNN_Interface(image,TEST_LAYERS)
     print(result.shape)
     FC_LAYER=[index,100,10]
@@ -115,11 +222,10 @@ if __name__ == "__main__":
     print(fcout.shape)
     print("=========================")
     with tf.Session() as sess:
-        
         sess.run(tf.global_variables_initializer())
         print(sess.run(result))
         writer=tf.summary.FileWriter("./log",tf.get_default_graph())
-        Matedata_Writer(writer,image,fcout,1)
+        Matedata_Writer(writer,{},fcout,1)
         
 
 
--- 机器学习/8.CIFAR-10 Large/Train-Model.py
+++ 机器学习/8.CIFAR-10 Large/Train-Model.py
@@ -2,6 +2,7 @@ from six.moves import cPickle as pickle
 import numpy as np 
 import os
 import platform
+import matplotlib.pyplot as plt 
 
 import tensorflow as tf
 import random
@@ -10,41 +11,39 @@ import time
 import NeuralNetwork as net
 
 BATCH_SIZE=5
-TRAINING_STEPS=20000
+TRAINING_STEPS=1000000
 DATASET_FOLDER='./dataset/'
 #检测频率
 CHECK_FREQUNCY=1000
 ACCURACY_TEST_DATA_UPDATE_FREQUENCY=5
 ACCURACY_TEST_BATCHSIZE=400
 #学习率设置
-LEARNING_RATE_BASE=1e-5
-LEARNING_RATE_DECAY_RATE=0.98
-LEARNING_RATE_DECAY_STEP=100
+LEARNING_RATE_BASE=tf.Variable(0.0001)
+LEARNING_RATE_DECAY_RATE=0.99
+LEARNING_RATE_DECAY_STEP=1000
 #正则化比率
 #REGULARIZATION_RATE=0.001
-REGULARIZATION_RATE=0.00000000001
+REGULARIZATION_RATE=0.00001
 #REGULARIZATION_RATE=0.000005
 
-DROPOUT_FORWARD_NETWORK=1
+DROPOUT_FORWARD_NETWORK=0.9
 
 IMAGE_SIZE=[32,32,3]
-
-#层1-? 全连接层
-Layer=[128,512,128,64,10]
 #总计训练次数
 global_step=tf.Variable(0,trainable=False)
-
+#层1-? 全连接层
+Layer=[64,2018,1024,512,128,10]
+#卷积层
 CNN_LAYERS=[
-    ["conv",[3,3,3,32],[1,1,1,1],True,"SAME"],
-    ["conv",[2,2,32,48],[1,1,1,1],True,"SAME"],
-    ["conv",[2,2,48,48],[1,1,1,1],True,"SAME"],
-    ["pool",[1,2,2,1],[1,2,2,1]],
-    ["conv",[3,3,48,80],[1,1,1,1],True,"SAME"],
-    ["pool",[1,2,2,1],[1,2,2,1]],
-    ["conv",[3,3,80,128],[1,1,1,1],True,"SAME"],
-    ["pool",[1,8,8,1],[1,8,8,1]],
+    ["conv",[3,3,3,32],[1,1,1,1],0.0001,"SAME",False],
+    ["conv",[3,3,32,48],[1,1,1,1],0.0001,"SAME",False],
+    ["maxpool",[1,3,3,1],[1,2,2,1]],
+    ["conv",[3,3,48,80],[1,1,1,1],0.01,"SAME",False],
+    ["avgpool",[1,3,3,1],[1,2,2,1]],
+    ["conv",[3,3,80,128],[1,1,1,1],0.01,"SAME",False],
+    ["conv",[3,3,128,256],[1,1,1,1],0.01,"SAME",False],
+    ["avgpool",[1,3,3,1],[1,2,2,1]],
 ]
-
 #输出格式串
 OUTPUT_FORMAT_STRING=">> Steps:{step: <5} Learning rate:{lr:.10f} Cross entropy:{ce:.5f} Regularization:{re:.5f}\n   Accuracy:{acc:.2%} \n   Accuracy in Train data:{accit:.2%}\n{srlt}"
 
@@ -68,12 +67,9 @@ class CIFAR10:
     def __load_CIFAR_batch(self,filename):
         """ load single batch of cifar """
         with open(filename, 'rb') as f:
-            datadict = self.__load_pickle(f)   # dict类型
-        X = datadict['data']        # X, ndarray, 像素值
-        Y = datadict['labels']      # Y, list, 标签, 分类
-        # reshape, 一维数组转为矩阵10000行3列。每个entries是32x32
-        # transpose，转置
-        # astype，复制，同时指定类型
+            datadict = self.__load_pickle(f)
+        X = datadict['data']
+        Y = datadict['labels']
         X = X.reshape(10000, 3, 32, 32).transpose(0,2,3,1).astype("float")
         Y = np.array(Y)
         return X, Y
@@ -128,58 +124,45 @@ class CIFAR10:
             self.Test['image'] , self.Test['label'] = self.__load_CIFAR_batch(os.path.join(ROOT, 'test_batch'))
             self.Test_Size=10000
 
+#输入数据
 keep_prob_holder=tf.placeholder(tf.float32)
 image=tf.placeholder(tf.float32,shape=[None]+IMAGE_SIZE,name='image')
 label_=tf.placeholder(tf.float32,shape=[None,Layer[-1]],name='label_')
-
+#网络层
 line_shape_result,len_shape_length=net.CNN_Interface(image,CNN_LAYERS,bias=False)
 Layer[0]=len_shape_length
 label=net.FC_Interface(line_shape_result,Layer,keep_prob_layer=keep_prob_holder)
-
+#测试输出
 label_to_num=tf.argmax(label,1)[0]
 label__to_num=tf.argmax(label_,1)[0]
 label_sampel=tf.nn.softmax(label[0])
-
 #学习率指数衰减
 learning_rate=tf.train.exponential_decay(LEARNING_RATE_BASE,global_step,LEARNING_RATE_DECAY_STEP,LEARNING_RATE_DECAY_RATE)
-#交叉熵
-cross_entropy=tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=label_,logits=label),name='loss')
-#正则化
-regularizer=tf.contrib.layers.l2_regularizer(REGULARIZATION_RATE)
-regularization_i=0.0
-weight=tf.get_collection("fc_weight")
-for i in weight:
-    regularization_i+=regularizer(i)
 #损失
-regularization=regularization_i
-#loss=regularization+cross_entropy
-loss=cross_entropy
+loss,cross_entropy,regularization=net.Softmax_Cross_Encropy_With_Regularization(label_,label,REGULARIZATION_RATE)
+#训练
 train_step=tf.train.GradientDescentOptimizer(learning_rate).minimize(loss,global_step=global_step)
-
 #正确率
 crrect_prediction=tf.equal(tf.argmax(label,1),tf.argmax(label_,1))
 accuracy=tf.reduce_mean(tf.cast(crrect_prediction,tf.float64))
+accuracy_traindict=tf.reduce_mean(tf.cast(crrect_prediction,tf.float64))
+tf.summary.scalar("Accuracy",accuracy)
 
+#储存
 saver=tf.train.Saver()
 
-
 with tf.Session() as sess:
-    summaries = tf.summary.merge_all()
-    writer=tf.summary.FileWriter("./log",tf.get_default_graph())
     print("=====================INFO====================")
-    print(\
-    "Learning rate base:{lrb}\n"\
-    "Learning rate decay rate:{lrdr}\n"\
-    "Learning rate decay step:{lrds}\n"
-    "Regularization rate:{regu}\n"\
-    "Keep prob:{drop}"\
-    .format(lrb=LEARNING_RATE_BASE,regu=REGULARIZATION_RATE,drop=DROPOUT_FORWARD_NETWORK,lrdr=LEARNING_RATE_DECAY_RATE,lrds=LEARNING_RATE_DECAY_STEP))
+    net.Info_Printer(learning_rate_base=LEARNING_RATE_BASE,
+    learning_rate_decay_rate=LEARNING_RATE_DECAY_RATE,
+    learning_rate_decay_step=LEARNING_RATE_DECAY_STEP,
+    regularization_rate=REGULARIZATION_RATE,
+    keep_prob_layer=DROPOUT_FORWARD_NETWORK
+    )
+    print("CNN output",len_shape_length)
     print("==================SET TIMER==================")
-    reply=input('Set time limit(s, unlimited: -1): ')
-    try:
-        TIMER=int(reply)
-    except ValueError:
-        TIMER=-1
+    timer=net.TrainTimer()
+    timer.getinput()
     print("====================LOAD=====================")
     #初始化
     reply=input('Load model?(y/n): ')
@@ -187,43 +170,67 @@ with tf.Session() as sess:
         saver.restore(sess,'./Tensorflow_model/model.ckpt')
     else:
         sess.run(tf.global_variables_initializer())
+    print("==============TRY LEARNING RATE==============")
+    reply=input("Try Learning rate?(y/n): ")
     print("=================INPUT DATA==================")
     dataset=CIFAR10(DATASET_FOLDER)
+    if reply=="y":
+        net.Learning_Rate_Search(LEARNING_RATE_BASE,train_step,accuracy,dataset,1e-5,lr_upper_bond=0.01,print_data=True,train_step=2000)
+        reply=input("Input a new learning rate: ")
+        sess.run(tf.global_variables_initializer())
+        new_lr=tf.assign(LEARNING_RATE_BASE,float(reply))
+        sess.run(new_lr)
+    ##写文件
+    summaries = tf.summary.merge_all()
+    writer=tf.summary.FileWriter("./log",tf.get_default_graph())
     print("===================RESULT====================")
     accuracy_test_count=0
     time_start=time.time()
+#    lr_manual=tf.assign(LEARNING_RATE_BASE,0.0005)
+#    sess.run(lr_manual)
     for i in range(TRAINING_STEPS):
-        #计时器
-        time_check=time.time()
-        if (time_check-time_start)>TIMER | TIMER!=-1:
-            break
-        #训练
-        feed_dict_train=dataset.nextbatch(BATCH_SIZE)
-        sess.run(train_step,feed_dict=feed_dict_train)
-        del feed_dict_train
-        #测试
-        if i%CHECK_FREQUNCY==0:
-            if accuracy_test_count==0:
-                accuracy_test_train_dict=dataset.nextbatch(ACCURACY_TEST_BATCHSIZE)
-                accuracy_test_dict=dataset.nextbatch(ACCURACY_TEST_BATCHSIZE,type='Test')
-                accuracy_test_count=ACCURACY_TEST_DATA_UPDATE_FREQUENCY
-            accuracy_test_count-=1
-            #print("Training step:",i,'Accuracy:',sess.run(accuracy,feed_dict=feed),sess.run(loss,feed_dict=feed),sess.run(learning_rate),answer(sess.run(y,feed_dict=feed)[0]),answer(sess.run(y_,feed_dict=feed)[0]))
-            #print("Steps:",i,'Accuracy:',"%.1f" % sess.run(accuracy,feed_dict=accuracy_test_dict),'Learning rate:',sess.run(learning_rate),sess.run(regularization,feed_dict=accuracy_test_dict),sess.run(cross_entropy,feed_dict=accuracy_test_dict))
-            print(OUTPUT_FORMAT_STRING.format(\
-                step=i,\
-                acc=sess.run(accuracy,feed_dict=accuracy_test_dict),\
-                lr=sess.run(learning_rate),\
-                ce=sess.run(cross_entropy,feed_dict=accuracy_test_dict),\
-                re=sess.run(regularization,feed_dict=accuracy_test_dict),\
-                accit=sess.run(accuracy,feed_dict=accuracy_test_train_dict),\
-                srlt=sess.run(label_sampel,feed_dict=accuracy_test_dict)),\
-                sess.run(label_to_num,feed_dict=accuracy_test_dict),\
-                sess.run(label__to_num,feed_dict=accuracy_test_dict))
-            #写Tensorboard信息
-            summ = sess.run(summaries, feed_dict=accuracy_test_dict)
-            writer.add_summary(summ, global_step=i)
-            net.Matedata_Writer(writer,accuracy_test_train_dict,train_step,i)
+        i=i
+        try:
+            #计时器
+            if timer.check==1:
+                break
+            #训练
+            feed_dict_train=dataset.nextbatch(BATCH_SIZE)
+            sess.run(train_step,feed_dict=feed_dict_train)
+            #测试
+            if i%CHECK_FREQUNCY==0:
+                if accuracy_test_count==0:
+                    accuracy_test_train_dict=dataset.nextbatch(ACCURACY_TEST_BATCHSIZE)
+                    accuracy_test_dict=dataset.nextbatch(ACCURACY_TEST_BATCHSIZE,type='Test')
+                    accuracy_test_count=ACCURACY_TEST_DATA_UPDATE_FREQUENCY
+                accuracy_test_count-=1
+                #print("Training step:",i,'Accuracy:',sess.run(accuracy,feed_dict=feed),sess.run(loss,feed_dict=feed),sess.run(learning_rate),answer(sess.run(y,feed_dict=feed)[0]),answer(sess.run(y_,feed_dict=feed)[0]))
+                #print("Steps:",i,'Accuracy:',"%.1f" % sess.run(accuracy,feed_dict=accuracy_test_dict),'Learning rate:',sess.run(learning_rate),sess.run(regularization,feed_dict=accuracy_test_dict),sess.run(cross_entropy,feed_dict=accuracy_test_dict))
+                print(OUTPUT_FORMAT_STRING.format(\
+                    step=i,\
+                    acc=sess.run(accuracy,feed_dict=accuracy_test_dict),\
+                    lr=sess.run(learning_rate),\
+                    ce=sess.run(cross_entropy,feed_dict=accuracy_test_dict),\
+                    re=sess.run(regularization,feed_dict=accuracy_test_dict),\
+                    accit=sess.run(accuracy_traindict,feed_dict=accuracy_test_train_dict),\
+                    srlt=sess.run(label_sampel,feed_dict=accuracy_test_dict)),\
+                    sess.run(label_to_num,feed_dict=accuracy_test_dict),\
+                    sess.run(label__to_num,feed_dict=accuracy_test_dict))
+                #写Tensorboard信息
+                summ = sess.run(summaries, feed_dict=accuracy_test_dict)
+                writer.add_summary(summ, global_step=i)
+                net.Matedata_Writer(writer,accuracy_test_train_dict,train_step,i)
+        except KeyboardInterrupt:
+            new_lr=input("New learning rate(-1 to quit): ")
+            if new_lr>0:
+                try:
+                    change_lr=tf.assign(LEARNING_RATE_BASE,float(new_lr))
+                    sess.run(change_lr)
+                except:
+                    print("Failed to change learning rate!")
+            else:
+                break
+
     writer.close()
     print("===================================")
     reply=input('Save?(y/n): ')
